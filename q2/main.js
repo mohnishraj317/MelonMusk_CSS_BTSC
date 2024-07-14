f17ad728f45159c13af390909ed527333e7d30c4
Mohnish Raj Mani <mohnishrajmani317@gmail.com>
2024-07-15 00:05:39 +0530

    added bg

--- q2/main.js
+++ q2/main.js
@@ -1,15 +1,27 @@
 const harryWithWand = document.querySelector(".harry-and-wand");
+const starsContainer = document.querySelector(".stars");
 
 document.addEventListener("click", e => {
-  const box = harryWithWand.getBoundingClientRect();
-  const xi = box.left;
-  const yi = box.top;
-
   const x = e.clientX;
   const y = e.clientY;
 
-  const theta = ~~Math.atan2(y - yi, x - xi);
-
   harryWithWand.style.setProperty("--x", x + "px");
   harryWithWand.style.setProperty("--y", y + "px");
-})
+});
+
+for (let i = 0; i < 25; i++) {
+  console.log("Hello world")
+  const star = document.createElement("div");
+  const size = Math.random() * 2 + .5;
+
+  star.className = "star";
+
+  star.style.cssText = `
+    --size: ${size}px;
+    --delay: ${i * 100}ms;
+    top: ${innerHeight * Math.random()}px;
+    left: ${innerWidth * Math.random()}px;
+  `;
+
+  starsContainer.append(star);
+}
